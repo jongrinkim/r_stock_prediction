384fd39b2c3c1c93100ecacec8cf606f19b328f0
Jongrin Kim <jongrinkim89@gmail.com>
2016-07-14 16:49:26 -0700

    shell file created that use wget to pull stock data

---
+++ Ray_input/ray_getStock.sh
@@ -0,0 +1,64 @@
+wget -O ADMP.csv "http://ichart.finance.yahoo.com/table.csv?s=ADMP&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O AMS.csv "http://ichart.finance.yahoo.com/table.csv?s=AMS&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O CGI.csv "http://ichart.finance.yahoo.com/table.csv?s=CGI&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O CLWT "http://ichart.finance.yahoo.com/table.csv?s=CLWT&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O DAC.csv "http://ichart.finance.yahoo.com/table.csv?s=DAC&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O DGLY.csv "http://ichart.finance.yahoo.com/table.csv?s=DGLY&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O DRD.csv "http://ichart.finance.yahoo.com/table.csv?s=DRD&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O ECTE.csv "http://ichart.finance.yahoo.com/table.csv?s=ECTE&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O EVRI.csv "http://ichart.finance.yahoo.com/table.csv?s=EVRI&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O HGG.csv "http://ichart.finance.yahoo.com/table.csv?s=HGG&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O HMNY.csv "http://ichart.finance.yahoo.com/table.csv?s=HMNY&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O ISNS.csv "http://ichart.finance.yahoo.com/table.csv?s=ISNS&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O KONA.csv "http://ichart.finance.yahoo.com/table.csv?s=KONA&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O NEWS.csv "http://ichart.finance.yahoo.com/table.csv?s=NEWS&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O REN.csv "http://ichart.finance.yahoo.com/table.csv?s=REN&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O STXS.csv "http://ichart.finance.yahoo.com/table.csv?s=STXS&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O TCCO.csv "http://ichart.finance.yahoo.com/table.csv?s=TCCO&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O RAD.csv "http://ichart.finance.yahoo.com/table.csv?s=RAD&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O XPLR.csv "http://ichart.finance.yahoo.com/table.csv?s=XPLR&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O IIN.csv "http://ichart.finance.yahoo.com/table.csv?s=IIN&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O CO.csv "http://ichart.finance.yahoo.com/table.csv?s=CO&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O AMCN.csv "http://ichart.finance.yahoo.com/table.csv?s=AMCN&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O OZM.csv "http://ichart.finance.yahoo.com/table.csv?s=OZM&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O AEG.csv "http://ichart.finance.yahoo.com/table.csv?s=AEG&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O TI.csv "http://ichart.finance.yahoo.com/table.csv?s=TI&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O KVHI.csv "http://ichart.finance.yahoo.com/table.csv?s=KVHI&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O JRJC.csv "http://ichart.finance.yahoo.com/table.csv?s=JRJC&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O PZN.csv "http://ichart.finance.yahoo.com/table.csv?s=PZN&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O NVDQ.csv "http://ichart.finance.yahoo.com/table.csv?s=NVDQ&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O SPCB.csv "http://ichart.finance.yahoo.com/table.csv?s=SPCB&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O FCEL.csv "http://ichart.finance.yahoo.com/table.csv?s=FCEL&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
+wget -O GASS.csv "http://ichart.finance.yahoo.com/table.csv?s=GASS&a=00&b=1&c=2003&d=07&e=1&f=2016&g=m&ignore=.csv"
+
